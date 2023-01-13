d67c0a96f7e5ae6c334b4483f2c6ac03c38f091f
VictorBDias <victor.batisttete@gmail.com>
2023-01-12 21:33:16 -0300

    add assets + initial config

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,10 @@ function App() {
 
   return (
     <div className="App">
-   <img src={idle} style={{maxHeight: 200, maxWidth: 200}}></img>
+      <a href="https://github.com/VictorBDias"><img style={{maxHeight: 20, position: 'absolute', top: 24, left: 24}} src="https://fontmeme.com/permalink/230113/0ed82b0f1c55ad4987393aa0b421b259.png" alt="name-lv-cap" border="0"/> </a> 
+      <div style={{display: 'grid'}}>
+        <img src={idle} style={{maxHeight: 200, maxWidth: 200, margin: 24}}></img>
+      </div>
     </div>
   )
 }
